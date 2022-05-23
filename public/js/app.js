e106092eda25f0c0db859d62112316d09f36c025
Hosea-Kibet <kibethosea6@gmail.com>
2022-05-23 15:19:29 +0300

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -4,33 +4,30 @@
 //     })
 // }))
 
-
-
 const weatherForm = document.querySelector("form");
-const searchItem = document.querySelector('input')
-const messageone= document.querySelector('#messsage1')
-const messagetwo=document.querySelector('#message2')
+const searchItem = document.querySelector("input");
+const messageone = document.querySelector("#messsage1");
+const messagetwo = document.querySelector("#message2");
 
-weatherForm.addEventListener('submit',(e)=>{
-    e.preventDefault()
-    const location = searchItem.value
-    
-    messageone.textContent= 'loading...'
-    messagetwo.textContent= ''
-    fetch("http://localhost:3000/weather?address="+ location).then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      //console.log(data.error);
-      messageone.textContent = data.error
-    } else {
-    // console.log(data.location);
-    messageone.textContent=data.location
-    // console.log(data.forecast);
-    messagetwo.textContent=data.forecast
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = searchItem.value;
+
+  messageone.textContent = "loading...";
+  messagetwo.textContent = "";
+  fetch("/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          //console.log(data.error);
+          messageone.textContent = data.error;
+        } else {
+          // console.log(data.location);
+          messageone.textContent = data.location;
+          // console.log(data.forecast);
+          messagetwo.textContent = data.forecast;
+        }
+      });
     }
-  });
+  );
 });
-
-    
-})
-
